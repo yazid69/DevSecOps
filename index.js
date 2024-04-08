9a4da10027e38dc2d178a1ab0951ca73bc4cd5c8
yazid69 <yazidlembirik69@outlook.fr>
2024-04-08 16:42:35 +0200

    deployment

---
+++ index.js
@@ -0,0 +1,19 @@
+document.getElementById('add-task-btn').addEventListener('click', function () {
+    const taskInput = document.getElementById('task-input');
+    if (taskInput.value.trim() !== '') {
+        const tasksUl = document.getElementById('tasks-ul');
+        const li = document.createElement('li');
+        li.textContent = taskInput.value;
+        li.addEventListener('click', function () {
+            this.classList.toggle('completed');
+        });
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Supprimer';
+        deleteBtn.addEventListener('click', function () {
+            tasksUl.removeChild(li);
+        });
+        li.appendChild(deleteBtn);
+        tasksUl.appendChild(li);
+        taskInput.value = ''; // Reset input field
+    }
+});
