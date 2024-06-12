923402a7f9ad41ec61b1f49458be0bbb32508d9c
yazid69 <yazidlembirik69@outlook.fr>
2024-06-12 11:11:27 +0200

    test retest comment and vercel without xss error

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-document.getElementById('add-task-btn').addEventListener('click', function () {
-    const taskInput = document.getElementById('task-input');
-    if (taskInput.value.trim() !== '') {
-        const tasksUl = document.getElementById('tasks-ul');
-        const li = document.createElement('li');
+// document.getElementById('add-task-btn').addEventListener('click', function () {
+//     const taskInput = document.getElementById('task-input');
+//     if (taskInput.value.trim() !== '') {
+//         const tasksUl = document.getElementById('tasks-ul');
+//         const li = document.createElement('li');
 
-        // Introducing a XSS vulnerability
-        li.innerHTML = taskInput.value;
+//         // Introducing a XSS vulnerability
+//         li.innerHTML = taskInput.value;
 
-        li.addEventListener('click', function () {
-            this.classList.toggle('completed');
-        });
-        const deleteBtn = document.createElement('button');
-        deleteBtn.textContent = 'Supprimer';
-        deleteBtn.addEventListener('click', function () {
-            tasksUl.removeChild(li);
-        });
-        li.appendChild(deleteBtn);
-        tasksUl.appendChild(li);
-        taskInput.value = '';
-    }
-});
+//         li.addEventListener('click', function () {
+//             this.classList.toggle('completed');
+//         });
+//         const deleteBtn = document.createElement('button');
+//         deleteBtn.textContent = 'Supprimer';
+//         deleteBtn.addEventListener('click', function () {
+//             tasksUl.removeChild(li);
+//         });
+//         li.appendChild(deleteBtn);
+//         tasksUl.appendChild(li);
+//         taskInput.value = '';
+//     }
+// });
